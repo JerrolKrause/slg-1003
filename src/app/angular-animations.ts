f6e62dfa31cc2a9e9aa71dd9ca5ac2787bf77127
Russell Stout <driftfox@hotmail.com>
2019-12-15 16:43:59 -0800

    Add animations to route changes and headings

---
+++ src/app/angular-animations.ts
@@ -0,0 +1,46 @@
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
+
+export const slideInAnimation = trigger('slideInAnimation', [
+  transition('* => *', [
+    query(':enter, :leave', style({
+      position: 'absolute',
+      width:'100%',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+    }), { optional: true }),
+    group([
+      query(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('0.5s ease-out', style({ transform: 'translateX(0%)' }))
+      ], { optional: true }),
+      query(':leave', [
+        style({ transform: 'translateX(0%)' }),
+        animate('0.5s ease-out', style({ transform: 'translateX(-100%)' })),
+      ], { optional: true }),
+    ])
+  ]),
+  // transition('* => *', [
+  //   query(':enter, :leave', style({
+  //     position: 'absolute',
+  //     width:'100%',
+  //     top: 0,
+  //     left: 0,
+  //     bottom: 0,
+  //     right: 0,
+  //   }), { optional: true }),
+  //   group([
+  //     query(':enter', [
+  //       style({ transform: 'translateX(-100%)' }),
+  //       animate('0.5s ease-out', style({ transform: 'translateX(0%)' }))
+  //     ], { optional: true }),
+  //     query(':enter', animateChild()),
+  //     query(':leave', [
+  //       style({ transform: 'translateX(0%)' }),
+  //       animate('0.5s ease-out', style({ transform: 'translateX(100%)' })),
+  //     ], { optional: true }),
+  //     query(':leave', animateChild()),
+  //   ])
+  // ])
+]);
--- src/app/components/project/masterpage/main/layout-main.component.ts
+++ src/app/components/project/masterpage/main/layout-main.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { slideInAnimation } from '../../../../angular-animations';
 
 @Component({
   selector: 'app-layout-main',
   templateUrl: './layout-main.component.html',
+  animations: [
+    slideInAnimation,
+  ]
 })
 export class LayoutMainComponent {
   constructor() {}
