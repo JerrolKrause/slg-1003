08f5876df9865c5b36eeb01602ea6b4772401e49
Jerrol Krause <jerrolkrause@gmail.com>
2020-01-10 11:13:47 -0800

    Localization

--- src/environments/defaults.ts
+++ src/environments/defaults.ts
@@ -8,7 +8,7 @@ export const defaults: EnvironmentConfig = {
   production: false,
   properties: {
     /** Name of application */
-    appName: 'Angular Starter',
+    appName: 'Sovereign Lending Group',
   },
   settings: {
     /** Enable service worker functionality */
